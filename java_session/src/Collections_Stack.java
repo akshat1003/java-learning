d3790f1b832049d77b050b9735d7feeb0a0ffde4
Akshat Aggarwal <akshat.aggarwal@loconav.com>
2023-07-08 17:21:48 +0530

    Implemented Collection_Stack

---
+++ java_session/src/Collections_Stack.java
@@ -0,0 +1,20 @@
+import java.util.Stack;
+
+public class Collections_Stack {
+    public static void main(String[] args)
+    {
+
+
+        Stack<String> animals=new Stack<>();
+        animals.push("Lion");
+        animals.push("Dog");
+        animals.push("Horse");
+        animals.push("Cheetah");
+        animals.push("Cat");
+        System.out.println(animals);
+//        System.out.println(animals.peek());
+//        System.out.println(animals.pop());
+//        System.out.println(animals);
+
+    }
+}
